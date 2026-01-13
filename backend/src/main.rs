6e65f211246d513c95f00dc0d21f6359bd14a752
Markusssssu <marik334477@mail.ru>
2026-01-13 13:20:22 +0500

    update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,14 +6,17 @@
 
 /*========use========*/
 use axum::{routing::{get}, Router};
+use tower_http::cors::CorsLayer;
 use std::error::Error;
 /*================== */
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> { 
 
+
     let app = Router::new()
-        .route("/api/health", get(health_check));
+    .route("/api/health", get(health_check))
+    .layer(CorsLayer::permissive()); 
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
     
